e696187c63522513442932b89180f943505f16b1
ToLive <seventyseventh@gmail.com>
2021-02-11 22:28:42 +0200

    Stage 4. Added test, cc code coverage

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { generateDiff } from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+describe('testing plain json', () => {
+  test('plain json compare', () => {
+    const filepath1 = getFixturePath('plain1.json');
+    const filepath2 = getFixturePath('plain2.json');
+
+    const testedString = generateDiff(filepath1, filepath2);
+    const referenceString = readFile('plainjsonResult.txt');
+
+    expect(testedString).toMatch(referenceString);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import path from 'path';
 import fs from 'fs';
 import _ from 'lodash';
 
+const generateDiff = (path1, path2) => {
+  const fullPathToFile1 = path.resolve(process.cwd(), path1);
+  const fullPathToFile2 = path.resolve(process.cwd(), path2);
+
+  const file1Content = JSON.parse(fs.readFileSync(fullPathToFile1));
+  const file2Content = JSON.parse(fs.readFileSync(fullPathToFile2));
+  const file1Keys = Object.keys(file1Content);
+  const file2Keys = Object.keys(file2Content);
+
+  const mergedFile = Object.values(_.union(file1Keys, file2Keys));
+
+  const resArray = _.sortBy(mergedFile).reduce((acc, key) => {
+    const file1value = file1Content[key];
+    const file2value = file2Content[key];
+
+    if (file1value === file2value) {
+      return [...acc, `   ${key}: ${file1value}`];
+    }
+
+    if (_.has(file1Content, key) && !file2value) {
+      return [...acc, `  - ${key}: ${file1value}`];
+    }
+
+    if (_.has(file1Content, key)
+        && _.has(file2Content, key)
+        && file1value !== file2value) {
+      return [...acc, `  - ${key}: ${file1value}`, `  + ${key}: ${file2value}`];
+    }
+
+    if (_.has(file2Content, key) && !file1value) {
+      return [...acc, `  + ${key}: ${file2value}`];
+    }
+
+    return acc;
+  }, []);
+
+  return `{ \n${resArray.join('\n')}\n}`;
+};
+
 const genDiff = () => {
   program.configureHelp({
     sortSubcommands: true,
@@ -16,45 +55,13 @@ const genDiff = () => {
     .arguments('<filepath1> <filepath2>')
     .option('-f, --format [type]', 'output format')
     .action((path1, path2) => {
-      const fullPathToFile1 = path.resolve(process.cwd(), path1);
-      const fullPathToFile2 = path.resolve(process.cwd(), path2);
+      const res = generateDiff(path1, path2);
 
-      const file1Content = JSON.parse(fs.readFileSync(fullPathToFile1));
-      const file2Content = JSON.parse(fs.readFileSync(fullPathToFile2));
-      const file1Keys = Object.keys(file1Content);
-      const file2Keys = Object.keys(file2Content);
-
-      const mergedFile = Object.values(_.union(file1Keys, file2Keys));
-
-      const resArray = _.sortBy(mergedFile).reduce((acc, key) => {
-        const file1value = file1Content[key];
-        const file2value = file2Content[key];
-
-        if (file1value === file2value) {
-          return [...acc, `   ${key}: ${file1value}`];
-        }
-
-        if (_.has(file1Content, key) && !file2value) {
-          return [...acc, `  - ${key}: ${file1value}`];
-        }
-
-        if (_.has(file1Content, key)
-            && _.has(file2Content, key)
-            && file1value !== file2value) {
-          return [...acc, `  - ${key}: ${file1value}`, `  + ${key}: ${file2value}`];
-        }
-
-        if (_.has(file2Content, key) && !file1value) {
-          return [...acc, `  + ${key}: ${file2value}`];
-        }
-
-        return acc;
-      }, []);
-
-      console.log(`{ \n${resArray.join('\n')}\n}`);
+      console.log(res);
     });
 
   program.parse();
 };
 
+export { generateDiff };
 export default genDiff;
