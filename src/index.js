c7cdd5b7bd24c372920fc2660b1b9f4528cc7c5e
ToLive <seventyseventh@gmail.com>
2021-03-08 18:34:10 +0200

    Refacroting

--- src/index.js
+++ src/index.js
@@ -97,7 +97,6 @@ const genDiff = () => {
   program
     .description('Compares two configuration files and shows a difference.')
     .arguments('<filepath1> <filepath2>')
-    .allowUnknownOption()
     .option('-f, --format [type]', 'output format', 'stylish')
     .action((path1, path2, args) => {
       const res = generateDiff(path1, path2, args.format);
